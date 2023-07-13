0add29a6ea8dd5e8ae463799f453a03ef04e68cf
Tomer Melnik <63450339+TomerMelnik@users.noreply.github.com>
2023-07-13 18:34:58 +0300

    Update start_docker_mac.sh

--- start_docker_mac.sh
+++ start_docker_mac.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 
+# Determine platform based on machine architecture
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "aarch64" ]; then
+    PLATFORM="linux/arm64"
+else
+    PLATFORM="linux/amd64"
+fi
+
+# Build and run Docker with platform determined above
 docker build -t eufs_sim .
 xhost + 
-sudo docker run -it --rm -v ~/.Xauthority:/root/.Xauthority --platform linux/amd64 --privileged --net=host -e DISPLAY=$DISPLAY eufs_sim:latest ./start_simulator.sh
+sudo docker run -it --rm -v ~/.Xauthority:/root/.Xauthority --platform $PLATFORM --privileged --net=host -e DISPLAY=$DISPLAY eufs_sim:latest ./start_simulator.sh
